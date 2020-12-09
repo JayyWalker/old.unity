2beabff27e28e2dc61a71a8766a092e37722bb6a
Jordan Walker <jordan@jayy.online>
2020-12-09 23:36:41 +0000

    build: Install eloquence and setup camelCasing

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        \Eloquence\EloquenceServiceProvider::class,
 
         /*
          * Application Service Providers...
